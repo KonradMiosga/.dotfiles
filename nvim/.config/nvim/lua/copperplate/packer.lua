026daa61898cf57e02157efda137426363eb87e6
Konrad Miosga <konrad.miosga@gmail.com>
2023-11-06 14:36:17 +0100

    removed nvim-ufo from nvim

--- nvim/.config/nvim/lua/copperplate/packer.lua
+++ nvim/.config/nvim/lua/copperplate/packer.lua
@@ -23,9 +23,6 @@ return require('packer').startup(function(use)
         end
     })
 
-    --code folding
-    use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
-
     use({
         "folke/trouble.nvim",
         config = function()
