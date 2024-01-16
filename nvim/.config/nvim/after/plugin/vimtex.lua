f44724256a92884a2ea1610e1158be6b3f65f152
Konrad Miosga <konrad.miosga@gmail.com>
2024-01-16 12:56:48 +0100

    sadf

--- nvim/.config/nvim/after/plugin/vimtex.lua
+++ nvim/.config/nvim/after/plugin/vimtex.lua
@@ -1,6 +1,7 @@
 -- PDF Viewer:
 vim.g['tex_flavor'] = 'latex'
 vim.g['vimtex_view_method'] = 'zathura'
+-- vim.g['vimtex_view_method'] = 'skim'
 vim.g['vimtex_view_skim_sync'] = 1
 vim.g['vimtex_view_skim_activate'] = 1
 
