6e29ebf32bc54d4b8b8ada685fd8ed92219dc66e
Konrad Miosga <konrad.miosga@gmail.com>
2025-10-17 09:35:14 +0200

    automated dev commit

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -1,11 +1,11 @@
 vim.keymap.set("n", "<leader>p", ":OpenPdf<CR>", { buffer = 0 })
 
 vim.cmd([[
-	setlocal wrapmargin=10
-	setlocal formatoptions+=t
-	setlocal linebreak
+	"setlocal wrapmargin=10
+	"setlocal formatoptions+=t
+	"setlocal linebreak
 	"setlocal spell
 	setlocal wrap
-	setlocal textwidth=100
-	setlocal colorcolumn=100
+	"setlocal textwidth=100
+	"setlocal colorcolumn=100
 ]])
