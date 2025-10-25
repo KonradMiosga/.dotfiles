4f28f314a57edfced00d1eda7d08a8dbac6471ea
Konrad Miosga <konrad.miosga@gmail.com>
2025-10-25 12:51:54 +0200

    sync

---
+++ qutebrowser/config.py
@@ -0,0 +1,34 @@
+config.load_autoconfig() # load settings done via the gui
+
+config.set("content.headers.user_agent",
+           "Mozilla/5.0 (X11; Linux x86_64) "
+           "AppleWebKit/537.36 (KHTML, like Gecko) "
+           "Chrome/121.0.0.0 Safari/537.36",
+           "https://accounts.google.com/*")
+
+# Adblocking info -->
+# For yt ads: place the greasemonkey script yt-ads.js in your greasemonkey folder (~/.config/qutebrowser/greasemonkey).
+# The script skips through the entire ad, so all you have to do is click the skip button.
+# Yeah it's not ublock origin, but if you want a minimal browser, this is a solution for the tradeoff.
+# You can also watch yt vids directly in mpv, see qutebrowser FAQ for how to do that.
+# If you want additional blocklists, you can get the python-adblock package, or you can uncomment the ublock lists here.
+c.content.blocking.enabled = True
+# c.content.blocking.method = 'adblock' # uncomment this if you install python-adblock
+# c.content.blocking.adblock.lists = [
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
