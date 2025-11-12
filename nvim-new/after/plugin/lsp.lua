12b8a424830ad145b5c1ae7207bd36f4f4cd1191
Konrad Miosga <konrad.miosga@gmail.com>
2025-11-12 22:12:47 +0100

    WIP nvim-new

--- nvim-new/after/plugin/lsp.lua
+++ nvim-new/after/plugin/lsp.lua
@@ -17,12 +17,28 @@ vim.lsp.config['tinymist'] = {
 }
 
 vim.diagnostic.config {
-  float = {
-    focusable = false,
-    style = 'minimal',
-    border = 'rounded',
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  signs = vim.g.have_nerd_font and {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  } or {},
+  virtual_text = {
     source = 'if_many',
-    header = '',
-    prefix = '',
+    spacing = 2,
+    format = function(diagnostic)
+      local diagnostic_message = {
+        [vim.diagnostic.severity.ERROR] = diagnostic.message,
+        [vim.diagnostic.severity.WARN] = diagnostic.message,
+        [vim.diagnostic.severity.INFO] = diagnostic.message,
+        [vim.diagnostic.severity.HINT] = diagnostic.message,
+      }
+      return diagnostic_message[diagnostic.severity]
+    end,
   },
 }
